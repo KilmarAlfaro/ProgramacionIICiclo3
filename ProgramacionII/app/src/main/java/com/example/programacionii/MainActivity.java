ed8076703887abfe6f2c6bf34482964e0a5cb7d0
kilma_sykdvfv <kilmaralfaro72@gmail.com>
2024-03-15 11:47:56 -0600

    SQL primera parte (1 error en lista amigos)

--- ProgramacionII/app/src/main/java/com/example/programacionii/MainActivity.java
+++ ProgramacionII/app/src/main/java/com/example/programacionii/MainActivity.java
@@ -1,59 +1,174 @@
 package com.example.programacionii;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Message;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
-
-
-
-    Button btnValo;
-    Spinner  spnValorant;
-    Button btnCarrito;
-
-
-
+    Button btn;
+    FloatingActionButton fab;
+    TextView tempVal;
+    String accion = "nuevo";
+    String id="";
+    String urlCompletaFoto;
+    Intent tomarFotoIntent;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spnValorant = findViewById(R.id.spnValorant);
-        btnValo = findViewById(R.id.btnValo);
-        btnValo.setOnClickListener(new View.OnClickListener() {
+
+        fab = findViewById(R.id.fabListarAmigos);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                switch (spnValorant.getSelectedItemPosition()){
-                case 0:
-                    Toast.makeText(getApplicationContext(), "No se ha agregado nada al carrito", Toast.LENGTH_LONG).show();
-                    break;
-                    case 1:
-                        Toast.makeText(getApplicationContext(), "Producto agregado al carrito", Toast.LENGTH_LONG).show();
-                        break;
-                    case 2:
-                        Toast.makeText(getApplicationContext(), "Producto agregado al carrito", Toast.LENGTH_LONG).show();
-                        break;
-                    case 3:
-                        Toast.makeText(getApplicationContext(), "Producto agregado al carrito", Toast.LENGTH_LONG).show();
-                        break;
-                    case 4:
-                        Toast.makeText(getApplicationContext(), "Producto agregado al carrito", Toast.LENGTH_LONG).show();
-                        break;
-
-                }
-
+                abrirActividad();
             }
         });
+        btn = findViewById(R.id.btnGuardarAgendaAmigos);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    tempVal = findViewById(R.id.txtnombre);
+                    String nombre = tempVal.getText().toString();
 
+                    tempVal = findViewById(R.id.txtdireccion);
+                    String direccion = tempVal.getText().toString();
 
+                    tempVal = findViewById(R.id.txtTelefono);
+                    String tel = tempVal.getText().toString();
 
+                    tempVal = findViewById(R.id.txtemail);
+                    String email = tempVal.getText().toString();
 
+                    tempVal = findViewById(R.id.txtdui);
+                    String dui = tempVal.getText().toString();
+
+                    DB db = new DB(getApplicationContext(), "",null, 1);
+                    String[] datos = new String[]{id,nombre,direccion,tel,email,dui, urlCompletaFoto};
+                    mostrarMsg(accion);
+                    String respuesta = db.administrar_amigos(accion, datos);
+                    if(respuesta.equals("ok")){
+                        Toast.makeText(getApplicationContext(), "Amigo guardado con exito", Toast.LENGTH_LONG).show();
+                        abrirActividad();
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Error al intentar guardar el amigo: "+ respuesta, Toast.LENGTH_LONG).show();
+                    }
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "Error: "+ e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        img = findViewById(R.id.btnImgAmigo);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tomarFotoAmigo();
+            }
+        });
+        mostrarDatosAmigos();
+    }
+    private void tomarFotoAmigo(){
+        tomarFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File fotoAmigo = null;
+        try{
+            fotoAmigo = crearImagenAmigo();
+            if( fotoAmigo!=null ){
+                Uri uriFotoamigo = FileProvider.getUriForFile(MainActivity.this,
+                        "com.ugb.controlesbasicos.fileprovider", fotoAmigo);
+                tomarFotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriFotoamigo);
+                startActivityForResult(tomarFotoIntent, 1);
+            }else{
+                mostrarMsg("No se pudo creaar la foto");
+            }
+        }catch (Exception e){
+            mostrarMsg("Error al abrir la camara: "+ e.getMessage());
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try{
+            if(requestCode==1 && resultCode==RESULT_OK){
+                Bitmap imageBitmap = BitmapFactory.decodeFile(urlCompletaFoto);
+                img.setImageBitmap(imageBitmap);
+            }else{
+                mostrarMsg("El usuario cancelo la toma de la foto");
+            }
+        }catch (Exception e){
+            mostrarMsg("Error al obtener la foto de la camara");
+        }
+    }
+    private File crearImagenAmigo() throws Exception{
+        String fechaHoraMs = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()),
+                fileName = "imagen_"+ fechaHoraMs +"_";
+        File dirAlmacenamiento = getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        if( dirAlmacenamiento.exists()==false ){
+            dirAlmacenamiento.mkdirs();
+        }
+        File imagen = File.createTempFile(fileName, ".jpg", dirAlmacenamiento);
+        urlCompletaFoto = imagen.getAbsolutePath();
+        return imagen;
+    }
+    private void mostrarDatosAmigos(){
+        try{
+            Bundle parametros = getIntent().getExtras();//Recibir los parametros...
+            accion = parametros.getString("accion");
+
+            if(accion.equals("modificar")){
+                String[] amigos = parametros.getStringArray("amigos");
+                id = amigos[0];
+
+                tempVal = findViewById(R.id.txtnombre);
+                tempVal.setText(amigos[1]);
+
+                tempVal = findViewById(R.id.txtdireccion);
+                tempVal.setText(amigos[2]);
+
+                tempVal = findViewById(R.id.txtTelefono);
+                tempVal.setText(amigos[3]);
+
+                tempVal = findViewById(R.id.txtemail);
+                tempVal.setText(amigos[4]);
+
+                tempVal = findViewById(R.id.txtdui);
+                tempVal.setText(amigos[5]);
+
+                urlCompletaFoto = amigos[6];
+                Bitmap imageBitmap = BitmapFactory.decodeFile(urlCompletaFoto);
+                img.setImageBitmap(imageBitmap);
+            }
+        }catch (Exception e){
+            mostrarMsg("Error al mostrar datos: "+ e.getMessage());
+        }
+    }
+    private void mostrarMsg(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+    private void abrirActividad(){
+        Intent abrirActividad = new Intent(getApplicationContext(), lista_amigos.class);
+        startActivity(abrirActividad);
     }
 }
